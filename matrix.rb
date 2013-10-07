cd47877576e170b363312624fe9a2b2ef0957e6b
Ramon Mesa <alu4429@cc418.(none)>
2013-10-07 17:33:12 +0100

    Suma implementada

--- matrix.rb
+++ matrix.rb
@@ -4,8 +4,23 @@
 #############DEFINICIÓN DE MATRICES##############
 #################################################
 
-a = [[0,1,2],[3,4,5],[6,7,8]]
-b = [[8,7,6],[5,4,3],[2,1,0]]
+a = [[1,2,3],[4,5,6],[7,8,9]]
+b = [[1,1,1],[2,2,2],[3,3,3]]
+c = [[0,0,0],[0,0,0],[0,0,0]]
+
+def suma(a,b,c)  
+  
+  for i in (0...3)
+    for j in (0...3)
+      c[i][j] = a[i][j] + b[i][j]
+    end
+  end
+
+print "La matriz SUMA es: #{c}"
+
+end
 
 puts "La matriz A es: #{a}"
 puts "La matriz B es: #{b}"
+puts suma(a,b,c)
+
