aa5ee719acad3f3256f603c878e57d20aef985a5
Daniel Gutiérrez <alu0100695595@ull.edu.es>
2013-10-07 18:21:32 +0100

    Multiplicacion implementada

--- matrix.rb
+++ matrix.rb
@@ -4,8 +4,22 @@
 #############DEFINICIÓN DE MATRICES##############
 #################################################
 
-a = [[0,1,2],[3,4,5],[6,7,8]]
-b = [[8,7,6],[5,4,3],[2,1,0]]
+a = [[1,2,3],[4,5,6],[7,8,9]]
+b = [[1,1,1],[2,2,2],[3,3,3]]
+d = [[0,0,0],[0,0,0],[0,0,0]]
 
-puts "La matriz A es: #{a}"
-puts "La matriz B es: #{b}"
+def mul(a,b,d)
+ 
+  for k in (0...3)
+    for i in (0...3)
+      for j in (0...3)
+	d[k][i] += a[k][j]*b[j][i]   
+	end
+      end
+    end
+    print "La matriz PRODUCTO es: #{d}"
+
+end
+
+puts mul(a,b,d)
+	                         
